5d608b74d74812f55da6bf444569a507529c7b4e
chskTg <beingmsd77@gmail.com>
2024-09-19 11:27:45 -0400

    setup project, done

---
+++ root/components/Login/Login.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import {SafeAreaView, Text, View} from 'react-native';
+
+const App = () => {
+  {
+    <SafeAreaView>
+      <View>
+        <Text>Login Page</Text>
+      </View>
+    </SafeAreaView>;
+  }
+};
+
+export default App;
---
+++ root/components/Profile/Profile.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import {SafeAreaView, Text, View} from 'react-native';
+
+const App = () => {
+  {
+    <SafeAreaView>
+      <View>
+        <Text>Profile Page</Text>
+      </View>
+    </SafeAreaView>;
+  }
+};
+
+export default App;
